e6557e0b1908dcaaa0ee98104d9d5a42f07c6797
jedixo <jake.dixon@my.jcu.edu.au>
2016-05-10 20:31:58 +1000

    fucking shit

--- musofuse/old php stuff/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['username']); // remove individual session var
-  unset($_SESSION['id']);
-  unset($_SESSION['paid']);
-  session_destroy();
-  header('location:../index.php'); // redirct to certain page now
-?>
