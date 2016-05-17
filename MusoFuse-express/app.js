72b7992070d5736a171265e5926691685f928c17
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-05-17 22:06:07 +1000

    added upload scripts to the profile.html *untested* should work though

--- MusoFuse-express/app.js
+++ MusoFuse-express/app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var path = require('path');
+var multer  =   require('multer');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + '.mp3');
+  }
+});
+var upload = multer({ storage : storage}).single('userFile');
 
 
 var app = express();
@@ -51,6 +61,16 @@ app.use(function (err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+//uploading files support
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
 });
 
 var server = app.listen(app.get('port'), function () {
